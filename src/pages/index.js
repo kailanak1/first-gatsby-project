65e5cdd6199fd279b46f9478ab547111abd60653
kailana <kailanakahawaii@gmail.com>
2020-06-29 20:39:42 -0700

    deleted unnecessary files

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,17 +1,9 @@
 import React from "react"
-import { Link } from "gatsby"
-
-import Layout from "../components/layout"
-import Image from "../components/image"
-import SEO from "../components/seo"
+import '../styles/styles.scss'
 
 const IndexPage = () => (
   
-  <Layout>
-    <SEO title="Home" />
-      <h1>Style Guide</h1>
-
-  </Layout>
+  <p>Home</p>
 )
 
 export default IndexPage
