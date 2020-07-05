9c125db8336b169a4e8e80c3b5d51fa7bfcb0f51
kailana <kailanakahawaii@gmail.com>
2020-07-04 20:17:15 -0700

    set up project square

--- src/components/project.js
+++ src/components/project.js
@@ -6,7 +6,7 @@ import { Link } from "gatsby";
 const Project = () => {
     const data = useStaticQuery(graphql`
     query {
-      kailanaProfile: file(relativePath: { eq: "kailana_profile.jpeg" }) {
+      davidpic: file(relativePath: { eq: "david-pic.jpg" }) {
         childImageSharp {
           fluid(maxWidth: 300) {
             ...GatsbyImageSharpFluid
@@ -19,13 +19,16 @@ const Project = () => {
         <div className="project-blurb" id="project">
         <div className="container">
             <div className="row">
-                <div>Web Apps</div>
              <div className="project-content">
                  <div className="project-square">
-                     <div className="project-title">Project 1</div>
-                     <div className="project-image">insert image here</div>
+                    <a href="https://david-quora-frontend.firebaseapp.com/" target="_blank" rel="noopener noreferrer">
+                        <div className="project-title">Project 1</div>
+                     <div className="project-image">
+                         <Img fluid={data.davidpic.childImageSharp.fluid}/>
+                     </div>
                      <div className="project-desc">Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</div>
-                 </div>
+                     </a>
+                 </div> 
                  <div className="project-square">
                      <div className="project-title">Project 2</div>
                  </div>
