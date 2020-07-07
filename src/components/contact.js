99a90938c9c1c124ebf61e75f555d7564594d5a8
kailana <kailanakahawaii@gmail.com>
2020-07-06 21:38:37 -0700

    changed height of input box

--- src/components/contact.js
+++ src/components/contact.js
@@ -19,21 +19,22 @@ const Contact = () => {
         <div className="contact" id="contact">
             <div className="container">
               <div className="contact-title">Contact</div>
-              <form name="contact" method="POST" data-netlify="true">
-                <p>
-                    <label>Your Name: <input type="text" name="name" /></label>   
-                </p>
-                <p>
-                    <label>Your Email: <input type="email" name="email" /></label>
-                </p>
-            
-                <p>
-                    <label>Message: <textarea name="message"></textarea></label>
-                </p>
-                <p>
-                    <button type="submit">Send</button>
-                </p>
+              <div className="contact-form">
+                <form name="contact" method="POST" data-netlify="true">
+                    <p>
+                        <label>Your Name: <input type="text" name="name" /></label>   
+                    </p>
+                    <p>
+                        <label>Your Email: <input type="email" name="email" /></label>
+                    </p>
+                    <p>
+                        <label>Message: <textarea name="message"></textarea></label>
+                    </p>
+                    <p>
+                        <button type="submit">Send</button>
+                    </p>
               </form>
+             </div>
             </div>
         </div>
     )
---
+++ src/components/social.js
@@ -0,0 +1,89 @@
+import React from "react"; 
+import {useStaticQuery, graphql} from "gatsby"; 
+import Img from "gatsby-image";
+import { Link } from "gatsby";
+
+const Social = () => {
+    const data = useStaticQuery(graphql`
+    query {
+      davidpic: file(relativePath: { eq: "david-pic.jpg" }) {
+        childImageSharp {
+          fluid(maxWidth: 300) {
+            ...GatsbyImageSharpFluid
+          }
+        }
+      }
+      stellarpic: file(relativePath: { eq: "stellar.png" }) {
+        childImageSharp {
+          fluid(maxWidth: 300) {
+            ...GatsbyImageSharpFluid
+          }
+        }
+      }
+      recipepic: file(relativePath: { eq: "recipe-app.png" }) {
+        childImageSharp {
+          fluid(maxWidth: 300) {
+            ...GatsbyImageSharpFluid
+          }
+        }
+      }
+      mealp: file(relativePath: { eq: "mealp.png" }) {
+        childImageSharp {
+          fluid(maxWidth: 300) {
+            ...GatsbyImageSharpFluid
+          }
+        }
+      }
+    }
+  `)
+    return (
+        <div className="project-blurb" id="project">
+        <div className="container">
+            <div className="row">
+             <div className="project-content">
+                 <div className="project-square">
+                    <a href="https://david-quora-frontend.firebaseapp.com/" target="_blank" rel="noopener noreferrer">
+                        <div className="project-title">Project 1</div>
+                     <div className="project-image">
+                         <Img fluid={data.davidpic.childImageSharp.fluid}/>
+                     </div>
+                     <div className="project-desc">Personal Website. Rails   React/Redux  EmailJS</div>
+                     </a>
+                 </div> 
+                 <div className="project-square">
+                     <a href="https://stellar-778e3.web.app/" target="_blank" rel="noopener noreferrer">
+                     <div className="project-title">Project 2</div>
+                     <div className="project-image">
+                         <Img fluid={data.stellarpic.childImageSharp.fluid}/>
+                     </div>
+                     <div className="project-desc">Astro calendar. Rails   React  APIs</div>
+                     </a>
+                 </div>
+                 <div className="project-square">
+                 <a href="https://github.com/kailanak1/recipe-blog-frontend" target="_blank" rel="noopener noreferrer">
+                     <div className="project-title">Project 3</div>
+                     <div className="project-image">
+                         <Img fluid={data.recipepic.childImageSharp.fluid}/>
+                     </div>
+                     <div className="project-desc">Recipe app. Rails React Boostrap</div>
+                     </a>
+                 </div>
+                 <div className="project-square">
+                     <a href="https://github.com/kailanak1/mealp" target="_blank" rel="noopener noreferrer">
+                     <div className="project-title">Project 4</div>
+                     <div className="project-image">
+                         <Img fluid={data.mealp.childImageSharp.fluid}/>
+                         <div className="project-desc">Yelp-like app. Rails </div>
+                     </div>
+                     </a>
+                 </div>
+             </div>
+            
+        
+        </div>
+        </div>
+    </div>
+    )
+}
+
+export default Social 
