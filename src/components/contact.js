8400ee8c74ed309f3169c4abc10774c86d3bd54a
kailana <kailanakahawaii@gmail.com>
2020-07-10 14:02:13 -0700

    testing form

--- src/components/contact.js
+++ src/components/contact.js
@@ -1,4 +1,4 @@
-import React from "react"; 
+import React, { useState } from "react"; 
 import {useStaticQuery, graphql} from "gatsby"; 
 import Img from "gatsby-image";
 import { Link } from "gatsby";
@@ -15,23 +15,43 @@ const Contact = () => {
       }
     }
   `)
+  const [state, setState] = React.useState({})
+
+  const handleChange = (e) => {
+    setState({ ...state, [e.target.name]: e.target.value })
+  }
+
+  const handleSubmit = (e) => {
+    e.preventDefault()
+    const form = e.target
+    fetch('/', {
+      method: 'POST',
+      headers: { 'Content-Type': 'application/x-www-form-urlencoded' },
+      body: encode({
+        'form-name': form.getAttribute('name'),
+        ...state,
+      }),
+    })
+      .then(() => navigate(form.getAttribute('action')))
+      .catch((error) => alert(error))
+  }
     return (
         <div className="contact" id="contact">
             <div className="container">
               <div className="contact-title">Contact</div>
               <div className="contact-form">
-                <form name="contact" method="POST" data-netlify="true">
+                <form name="contact" method="POST" data-netlify="true" onSubmit={handleSubmit}>
                     <p>
-                        <label>Your Name: <input type="text" name="name" /></label>   
+                        <label>Your Name: <input type="text" name="name" onChange={handleChange}/></label>   
                     </p>
                     <p>
-                        <label>Your Email: <input type="email" name="email" /></label>
+                        <label>Your Email: <input type="email" name="email" onChange={handleChange}/></label>
                     </p>
                     <p>
-                        <label>Message: <textarea name="message"></textarea></label>
+                        <label>Message: <textarea name="message" onChange={handleChange}></textarea></label>
                     </p>
                     <p>
-                        <button className="submit-button" type="submit">Send</button>
+                        <button className="submit-button" type="submit" >Send</button>
                     </p>
               </form>
              </div>
